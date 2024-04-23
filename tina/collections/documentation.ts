cd18bf765f18761d36c7f236142f39831e3b9f97
Kirill <kirill970616@gmail.com>
2024-04-23 18:12:09 +0500

    some

--- tina/collections/documentation.ts
+++ tina/collections/documentation.ts
@@ -1,15 +1,10 @@
 import { Collection } from "tinacms";
-import { Record } from "immutable";
-import { boolean, string } from "yup";
 
 export const DocumentationCollection: Collection = {
   name: 'documentation',
   label: 'Документация',
   path: 'content/documentation',
   format: 'mdx',
-  ui: {
-    router: (props) => '/documentation'
-  },
   fields: [
     {
       type: 'string',
@@ -17,61 +12,8 @@ export const DocumentationCollection: Collection = {
       label: 'Заголовок'
     },
     {
-      name: 'blocks',
-      label: 'Разделы',
-      type: "object",
-      list: true,
-      templates: [
-        {
-          name: 'desc',
-          label: 'Описание',
-          fields: [
-            {
-              name: 'string',
-              type: 'string',
-              label: 'Описание',
-              ui: {
-                component: 'textarea'
-              }
-            }
-          ]
-        },
-        {
-          name: 'checklists',
-          label: 'Сценарии работы',
-          fields: [
-            {
-              name: 'scenario',
-              type: 'rich-text',
-              isBody: true,
-              label: "Сценарии",
-              templates: [
-                {
-                  name: 'SwaggerLink',
-                  label: 'Ссылка на сваггер',
-                  fields: [
-                    {
-                      name: 'link',
-                      type: 'string'
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          name: 'events',
-          label: 'Структура событий',
-          fields: [
-            {
-              name: 'event',
-              label: 'События',
-              type: 'rich-text'
-            }
-          ]
-        }
-      ]
+      type: 'rich-text',
+      name: 'body'
     }
   ]
 }
